7ee4e29663eeb54f46723d8b3bf12a8431254222
jdnourse <jdnourse@me.com>
2023-03-17 17:03:03 -0700

    Last time

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -26,14 +26,21 @@ describe('Launches API', () => {
     const completeLaunchData = {
       mission: 'USS Enterprise',
       rocket: 'NCC 1701-D',
-      destination: 'Kepler-1652 b',
+      destination: 'Kepler-62 f',
       launchDate: 'January 4, 2028',
     };
 
     const launchDataWithoutDate = {
       mission: 'USS Enterprise',
       rocket: 'NCC 1701-D',
-      destination: 'Kepler-1652 b',
+      destination: 'Kepler-62 f',
+    };
+
+    const launchDataWithInvalidDate = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701-D',
+      destination: 'Kepler-62 f',
+      launchDate: 'zoot',
     };
 
     test('It should respond with 201 created', async () => {
